fde8a09f9b9bfbdf3c3537b77910189ca6dcd818
Martin Barria Joost <martinjoostw@gmail.com>
2020-05-10 22:07:31 -0600

    dom console

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { connect } from 'twilio-video';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'ng-twilio';
+  room: any;
+  name: string;
+  loading: boolean;
+  
+  
+  joinRoom(room){
+    this.loading = true;
+    this.name = room;
+    connect('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2UwNzc2ZDdlYmExYmEzOGM0ZTBjZjk1NzAxODFmM2VlLTE1ODkxNjA2MTEiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJtYXJ0aW4iLCJ2aWRlbyI6e319LCJpYXQiOjE1ODkxNjA2MTEsImV4cCI6MTU4OTE3NTAxMSwiaXNzIjoiU0tlMDc3NmQ3ZWJhMWJhMzhjNGUwY2Y5NTcwMTgxZjNlZSIsInN1YiI6IkFDNGY0OTUyNmUwMjkyNmE3NzQxMTg2ZDY5MDdhYzk3NmIifQ.B8ur01S1xxQdWlQYW7npjFwWWpx1mP3oEt9z5wOYl4s', 
+    { name: this.name })
+    .then( (room) => {
+      this.loading = false;
+      
+      this.room = room;
+
+    }, error => {
+      console.error(`Unable to connect to Room: ${error.message}`);
+    });
+    
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
@@ -10,7 +16,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatProgressSpinnerModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
