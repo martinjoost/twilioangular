0904303d3083a1fa3561924869b3ce8fd2a502bd
Martin Barria Joost <martinjoostw@gmail.com>
2020-05-10 22:30:11 -0600

    show console dom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { connect } from 'twilio-video';
+import { connect, TrackNameCharsInvalidError } from 'twilio-video';
 
 
 @Component({
@@ -9,24 +9,26 @@ import { connect } from 'twilio-video';
 })
 export class AppComponent {
   title = 'ng-twilio';
-  room: any;
-  name: string;
-  loading: boolean;
-  
-  
-  joinRoom(room){
-    this.loading = true;
-    this.name = room;
-    connect('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2UwNzc2ZDdlYmExYmEzOGM0ZTBjZjk1NzAxODFmM2VlLTE1ODkxNjA2MTEiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJtYXJ0aW4iLCJ2aWRlbyI6e319LCJpYXQiOjE1ODkxNjA2MTEsImV4cCI6MTU4OTE3NTAxMSwiaXNzIjoiU0tlMDc3NmQ3ZWJhMWJhMzhjNGUwY2Y5NTcwMTgxZjNlZSIsInN1YiI6IkFDNGY0OTUyNmUwMjkyNmE3NzQxMTg2ZDY5MDdhYzk3NmIifQ.B8ur01S1xxQdWlQYW7npjFwWWpx1mP3oEt9z5wOYl4s', 
-    { name: this.name })
-    .then( (room) => {
-      this.loading = false;
-      
-      this.room = room;
+  loading:boolean;
+  roomData: any;
 
+
+
+  joinRoom(roomName){
+    this.loading = true;
+    
+    const { connect } = require('twilio-video');
+
+    connect('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImN0eSI6InR3aWxpby1mcGE7dj0xIn0.eyJqdGkiOiJTS2UwNzc2ZDdlYmExYmEzOGM0ZTBjZjk1NzAxODFmM2VlLTE1ODkxNzA2NzEiLCJncmFudHMiOnsiaWRlbnRpdHkiOiJtYXJ0aW4iLCJ2aWRlbyI6e319LCJpYXQiOjE1ODkxNzA2NzEsImV4cCI6MTU4OTE4NTA3MSwiaXNzIjoiU0tlMDc3NmQ3ZWJhMWJhMzhjNGUwY2Y5NTcwMTgxZjNlZSIsInN1YiI6IkFDNGY0OTUyNmUwMjkyNmE3NzQxMTg2ZDY5MDdhYzk3NmIifQ.198L6hDrhEjrMGYccjK9qIU9L-zieKYr9h2PR34YvTI', 
+    { name: roomName.value })
+    .then(room => {
+      this.roomData = room;
+      this.loading = false;
+      room.on('participantConnected', participant => {
+        console.log(`A remote Participant connected: ${participant}`);
+      });
     }, error => {
       console.error(`Unable to connect to Room: ${error.message}`);
     });
-    
   }
 }
